2520d17ad822ce35534750718c1f466536ecc531
Amna Qaisar <f23bscs034@kinnaird.edu.pk>
2026-04-23 05:18:09 +0500

    Step1: Saving the HTML by retrieving all product cards from multiple pages

---
+++ locating_multiple.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+query = "laptop"
+file = 0
+for i in range(1, 10):
+    driver.get(
+        f"https://www.amazon.com/s?k={query}&page={i}&crid=2NFNV25TJ6CRA&sprefix=laptop%2Caps%2C428&ref=nb_sb_noss_1"
+    )
+
+    elems = driver.find_elements(By.CLASS_NAME, "puis-card-container")
+    print(f"{len(elems)} items found")
+    for elem in elems:
+        d = elem.get_attribute("outerHTML")
+        with open(f"data/{query}_{file}.html", "w", encoding="utf-8") as f:
+            f.write(d)
+            file += 1
+
+    time.sleep(2)
+driver.close()
